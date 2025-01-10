e08575293b3721fe1a13552eb7f2e76ae080d78c
Javier Valladares Alonso <javierprofesionalwork@gmail.com>
2025-01-10 16:07:12 -0500

    Update projects.js

--- web/src/utils/projects.js
+++ web/src/utils/projects.js
@@ -28,6 +28,33 @@ export const projects = [
     ,  deploy: 'https://chat-ai-pink-six.vercel.app'
 
     },
+
+
+ {
+    name: 'Página Limegroup marketing',
+    img: '/assets/Captura de pantalla (134).png',
+    tecnologies: [
+   
+      {
+        name: 'JavaScript',
+        url: 'https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/javascript.png'
+      },
+      {
+        name: 'HTML5',
+        url: 'https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/html.png'
+      }
+     {
+        name: 'CSS3',
+        url: 'https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/css.png'
+      }
+    ],
+    description:
+      'Página para promocionar servicios de la empresa analisis seo con GSC ,empleo de Squarespace y programación frontend en casos específicos',
+    
+      url: 'https://github.com/programing-maste'
+    ,  deploy: 'https://limegroupmarketing.com'
+
+    },
   {
     name: 'App de Comida rápida',
     img: '/assets/Captura de pantalla (34).png',
